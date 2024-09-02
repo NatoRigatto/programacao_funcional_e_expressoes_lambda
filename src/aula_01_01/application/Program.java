09ed6e3e836854f1d1759fbd826c1cdad5330738
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-02 18:37:07 -0300

    Anonymous class

--- src/aula_01_01/application/Program.java
+++ src/aula_01_01/application/Program.java
@@ -1,24 +1,33 @@
 package aula_01_01.application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-import aula_01_01.entities.MyComparator;
 import aula_01_01.entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.0));
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
+
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+
+		};
 		
-		list.sort(new MyComparator());
-		
-		for(Product p : list) {
+		list.sort(comp);
+
+		for (Product p : list) {
 			System.out.println(p);
 		}
 
