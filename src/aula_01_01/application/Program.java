d2635c56c5fee3f7ce41d25b0662a32f1c22a331
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-02 18:42:52 -0300

    Lambda 3

--- src/aula_01_01/application/Program.java
+++ src/aula_01_01/application/Program.java
@@ -1,7 +1,6 @@
 package aula_01_01.application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import aula_01_01.entities.Product;
@@ -16,9 +15,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
 
-		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-		list.sort(comp);
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
 		for (Product p : list) {
 			System.out.println(p);
