5990857db6754c1806e00d838a08c5a9afc0369f
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-04 13:40:37 -0300

    Method reference static

--- src/aula_03/application/Program.java
+++ src/aula_03/application/Program.java
@@ -5,21 +5,20 @@ import java.util.List;
 import java.util.Locale;
 
 import aula_03.entities.Product;
-import aula_03.util.ProductConsumer;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
-		
+
 		List<Product> list = new ArrayList<>();
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		list.forEach(new ProductConsumer());
+
+		list.forEach(Product::staticProductConsumer);
 		list.forEach(System.out::println);
 
 	}
