ac59d4b3b805409dae6b5dafd6d99e194a7042ae
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-04 15:01:01 -0300

    Function implementation

---
+++ src/aula_04/application/Program.java
@@ -0,0 +1,28 @@
+package aula_04.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import aula_04.entities.Product;
+import aula_04.util.ProductFunction;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		List<String> name = list.stream().map(new ProductFunction()).collect(Collectors.toList());
+		name.forEach(System.out::println);
+
+	}
+
+}
