54cdabf61ccca402ace98bd2019f4d8157060ac0
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-04 15:05:09 -0300

    Declared lambda expression

--- src/aula_04/application/Program.java
+++ src/aula_04/application/Program.java
@@ -3,6 +3,7 @@ package aula_04.application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import aula_04.entities.Product;
@@ -18,8 +19,10 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
 
-		List<String> name = list.stream().map(Product::nonStaticProductFunction).collect(Collectors.toList());
+		List<String> name = list.stream().map(func).collect(Collectors.toList());
 		name.forEach(System.out::println);
 
 	}
